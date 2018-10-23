a689fefad4f5ec24e438eaa95b96e74ef623b0a8
Nishit Bharatkumar Ramoliya <n9915567@qut.edu.au>
2018-10-23 17:59:20 +1000

    Add project files.

---
+++ Travel/Customer.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel
+{
+    class Customer
+    {
+        //Declare and Initialise class parameters
+        private static int arraySize = 0;
+        private static int[] CustomerAge = new int[arraySize];
+        private static string[] CustomerFname = new string[arraySize];
+        private static string[] CustomerLname = new string[arraySize];
+        private static int[] CustomerPassport = new int[arraySize];
+        private static double[] CustomerPrice = new double[arraySize];
+        private static string[] CustomerTitle = new string[arraySize];
+        private double KidDiscount = 0.9;
+        private double SeniorDiscount = 0.8;
+
+        //Constructor
+        public Customer() { }
+        public Customer(int size)
+        {
+            CustomerAge = new int[size];
+            CustomerTitle = new string[size];
+            CustomerFname = new string[size];
+            CustomerLname = new string[size];
+            CustomerPassport = new int[size];
+            CustomerPrice = new double[size];
+        }
+        //Propterties of class
+        public int[] Customer_Age
+        {
+            get { return CustomerAge; }
+            set { CustomerAge = value; }
+        }
+        public string[] Customer_Title
+        {
+            get { return CustomerTitle; }
+            set { CustomerTitle = value; }
+        }
+        public string[] Customer_Fname
+        {
+            get { return CustomerFname; }
+            set { CustomerFname = value; }
+        }
+        public string[] Customer_Lname
+        {
+            get { return CustomerLname; }
+            set { CustomerLname = value; }
+        }
+        public int[] CustomerPassNo
+        {
+            get { return CustomerPassport; }
+            set { CustomerPassport = value; }
+        }
+        public double[] CustomerTicket
+        {
+            get { return CustomerPrice; }
+            set { CustomerPrice = value; }
+        }
+        //user-define function for calculating price of ticket
+        public void CalculatePrice()
+        {
+            for(int i = 0; i < CustomerFname.Length; i++)
+            {
+                if (CustomerAge[i] < 3)
+                {
+                    CustomerPrice[i] = 0;
+                }
+                else if (CustomerAge[i] >= 3 && CustomerAge[i] < 18)
+                {
+                    CustomerPrice[i] = CustomerPrice[i] * KidDiscount;
+                }
+      
+                else if (CustomerAge[i] >= 65)
+                {
+                    CustomerPrice[i] = CustomerPrice[i] * SeniorDiscount;
+                }
+            }
+        }
+        //Display all infomartion in tabular format
+        public void display()
+        {
+            double total = 0;
+            for(int j=0;j<CustomerPassNo.Length; j++)
+            {
+                total += CustomerPrice[j];
+            }
+            Console.WriteLine("\n-----------------------------------------------------------");
+            Console.WriteLine("\t\tName\tPassportNo.\tFare");
+            for(int i = 0; i < CustomerPassNo.Length; i++)
+            {
+                Console.WriteLine("-----------------------------------------------------------");
+                Console.WriteLine("Passenger"+(i+1)+"\t"+CustomerTitle[i]+""+CustomerFname[i]+""+CustomerLname[i]+"\t"+CustomerPassport[i]+"\t\t"+CustomerTicket[i]);
+            }
+            Console.WriteLine("-----------------------------------------------------------");
+            Console.WriteLine("\t\t\tNet amount: {0}", total);
+        }
+    }
+}
---
+++ Travel/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Travel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int passenger;  //Decalre local variable
+            bool check;     //Declare boolean operator
+            Console.WriteLine("Enter number of passenger: ");
+            check = int.TryParse(Console.ReadLine(), out passenger);    //ask user to enter passenger
+            while (check == false || passenger == 0 || passenger < 0)   //check input whether it is valid or not
+            {
+                Console.WriteLine("Invalid Input");     //display appropriate message
+                Console.WriteLine("Enter number of passenger: ");   
+                check = int.TryParse(Console.ReadLine(), out passenger);
+            }
+            Customer customer = new Customer(passenger);    //Create object of Customer class
+            for (int i = 0; i < passenger; i++)
+            {
+                Console.WriteLine("-----------Customer {0}-----------", i+1);    //Enter details of customer
+                customer.Customer_Age[i] = getAge();            //Call getAge() to enter age
+                customer.Customer_Title[i] = getTitle();
+                customer.Customer_Fname[i] = getInfo("First");  //Call getInfo() to enter First name
+                customer.Customer_Lname[i] = getInfo("Last");   //Call getInfo() to enter Last name
+                customer.CustomerTicket[i] = getAirfare();      //Call getAirfare() to enter Airfare
+                customer.CalculatePrice();                      //Calculate airfare per passenger
+                customer.CustomerPassNo[i] = getPassport();     //Call getPassport() to get passport number
+                for (int j = 0; j < i; j++)
+                {
+                    while (customer.CustomerPassNo[i] == customer.CustomerPassNo[j])    //check passport number is not repeated
+                    {
+                        Console.WriteLine("Passport already exists");
+                        customer.CustomerPassNo[i] = getPassport();
+                    }
+                    break;
+                }
+                
+            }
+            customer.display(); //call display method from customer class
+            Console.ReadKey();
+            Console.Clear();
+        }
+        //function for getting age from user
+        public static int getAge()
+        {
+            int age;
+            bool check;            
+            Console.WriteLine("Enter age: ");
+            check = int.TryParse(Console.ReadLine(), out age);
+            while (check == false || age == 0 || age < 0)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine("Enter age: ");
+                check = int.TryParse(Console.ReadLine(), out age);
+            }
+            return age;
+        }
+        //function for getting AirFare from user
+        public static double getAirfare()
+        {
+            double fare;
+            bool check;
+            Console.WriteLine("Enter Airfare: ");
+            check = double.TryParse(Console.ReadLine(), out fare);
+            while (check == false || fare < 0)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine("Enter Airfare: ");
+                check = double.TryParse(Console.ReadLine(), out fare);
+            }
+            return fare;
+        }
+        //function for getting first and last name
+        public static string getInfo(string what)
+        {
+            string name; 
+            Console.WriteLine("Enter {0} name: ", what);
+            name = Console.ReadLine();
+            while(string.IsNullOrEmpty(name))
+            {
+                Console.WriteLine("Name cannot be empty");
+                Console.WriteLine("Enter {0} name: ", what);
+                
+                name = Console.ReadLine();
+            }
+            return name;
+        }
+        //function for getting Passport number
+        public static int getPassport()
+        {
+            bool check;
+            int number;
+            Console.WriteLine("Enter Passport number: ");
+            check = int.TryParse(Console.ReadLine(), out number);
+            while (check == false || number < 0)
+            {
+                Console.WriteLine("Invalid input");
+                Console.WriteLine("Enter Passport number: ");
+                check = int.TryParse(Console.ReadLine(), out number);
+            }
+            return number;
+        }
+        //function for getting title
+        public static string getTitle()
+        {
+            string title;
+            Console.WriteLine("Get Title: ");
+            title = Console.ReadLine();
+            while (string.IsNullOrEmpty(title))
+            {
+                Console.WriteLine("Name cannot be empty");
+                Console.WriteLine("Get Title: ");
+                title = Console.ReadLine();
+            }
+            return title;
+        }
+    }
+}
